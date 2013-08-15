126cd851e272c6e1885e4b3ba42951c3bca5becb
Tyom Semonov <tyom@state.com>
2013-08-15 16:13:13 +0100

    fixup! WIP: Add support for CasperJS

--- capture.js
+++ capture.js
@@ -1,19 +1,20 @@
 var system = require('system');
 var casper = require('casper').create({
-	verbose: true,
-	logLevel: 'debug'
+  verbose: true,
+  logLevel: 'debug'
 });
 
 var url = casper.cli.args[0];
 var viewportWidth = casper.cli.args[1];
 var imageName = casper.cli.args[2];
-
+var screenAspectRatio = 4/3;
+var domain = url.match(/https?:\/\/([\w]+:[\w]+@)?([\w]+\.)?(?:[\w]+\.)(?:\.?[\w]{2,})+(:[\d]+)?/)[0];
 
 casper.start();
-casper.viewport(viewportWidth, 1500)
 	.thenOpen(url)
 	.then(function() {
 		this.capture(imageName);
 	});
+casper.viewport(viewportWidth, viewportWidth/screenAspectRatio)
 
 casper.run();
